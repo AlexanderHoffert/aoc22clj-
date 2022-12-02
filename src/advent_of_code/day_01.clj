24f4b3c45d3deb325110de8e70f47fea0c407169
Alexander Hoffert <alexander.hoffert@sap.com>
2022-12-02 06:30:28 +0100

    day 1

--- src/advent_of_code/day_01.clj
+++ src/advent_of_code/day_01.clj
@@ -1,11 +1,33 @@
 (ns advent-of-code.day-01)
+(require '[clojure.string :as string])
+
+(defn read-input [input]
+  (string/split input #"\n"))
+
+(defn get-sums [food]
+  (->> food
+       (map #(Integer/parseInt %))
+       (reduce +)))
+
+(defn parse-input [input]
+  (->> input
+       read-input
+       (partition-by #(= "" %))
+       (filter #(not (= "" (first %))))
+       (map get-sums)
+       (sort)))
 
 (defn part-1
   "Day 01 Part 1"
   [input]
-  input)
+  (->> input
+       parse-input
+       last))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  input)
+  (->> input
+       parse-input
+       (take-last 3)
+       (reduce +)))
