467211716e8b03b109665dee5b5dcd68539e022b
Alexander Hoffert <alexander.hoffert@sap.com>
2022-12-08 10:30:35 +0100

    day 6

--- src/advent_of_code/day_06.clj
+++ src/advent_of_code/day_06.clj
@@ -1,11 +1,20 @@
 (ns advent-of-code.day-06)
 
+(defn get-marker [input size]
+  (let [windows (partition size 1 input)]
+    (loop [position size
+           current (first windows)
+           rest (drop 1 windows)]
+      (if (apply distinct? current)
+        position
+        (recur (+ 1 position) (first rest) (drop 1 rest))))))
+
 (defn part-1
   "Day 06 Part 1"
   [input]
-  input)
+  (get-marker input 4))
 
 (defn part-2
   "Day 06 Part 2"
   [input]
-  input)
+  (get-marker input 14))
--- test/advent_of_code/day_06_test.clj
+++ test/advent_of_code/day_06_test.clj
@@ -3,10 +3,22 @@
             [advent-of-code.day-06 :refer [part-1 part-2]]
             [clojure.java.io :refer [resource]]))
 
-(deftest part1
-  (let [expected nil]
+(deftest part1-base
+  (let [expected 7]
     (is (= expected (part-1 (slurp (resource "day-06-example.txt")))))))
 
+(deftest part1-1
+  (is (= 5 (part-1 "bvwbjplbgvbhsrlpgdmjqwftvncz"))))
+
+(deftest part1-2
+  (is (= 6 (part-1 "nppdvjthqldpwncqszvftbrmjlhg"))))
+
+(deftest part1-3
+  (is (= 10 (part-1 "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"))))
+
+(deftest part1-4
+  (is (= 11 (part-1 "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))))
+
 (deftest part2
-  (let [expected nil]
+  (let [expected 19]
     (is (= expected (part-2 (slurp (resource "day-06-example.txt")))))))
